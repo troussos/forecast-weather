f54f99f3d8ea2f1987f005cdfa357d568c3f9049
Tyler Roussos <tylerroussos@gmail.com>
2014-01-21 20:57:01 -0800

    Setup a basic view.
    Setup a basic view for the initial landing screen. Simple icon and temperature are displayed, need to setup both the geolocation API now as well as the JSON call to forecast.io for the weather data.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,64 @@
 #include <pebble.h>
 
-Window *my_window;
-TextLayer *text_layer;
+static Window *window;
+static BitmapLayer *icon_layer;
+static TextLayer *temp_layer;
+static GBitmap *icon_bitmap = NULL;
 
-void handle_init(void) {
-	  my_window = window_create();
+static const uint32_t WEATHER_ICONS[] = {
+    RESOURCE_ID_IMAGE_CLOUD //0
+};
 
-	  text_layer = text_layer_create(GRect(0, 0, 144, 20));
+static void window_load(Window *window) {
+    Layer *window_layer = window_get_root_layer(window);
+
+    icon_layer = bitmap_layer_create(GRect(22, 16, 100, 74));
+	icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[0]);
+    bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
+    layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
+
+    temp_layer = text_layer_create(GRect(0, 95, 144, 68));
+    text_layer_set_text_color(temp_layer, GColorWhite);
+    text_layer_set_background_color(temp_layer, GColorClear);
+    text_layer_set_font(temp_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+    text_layer_set_text_alignment(temp_layer, GTextAlignmentCenter);
+	text_layer_set_text(temp_layer, "-25\u00B0F");
+    layer_add_child(window_layer, text_layer_get_layer(temp_layer));
+}
+//FONT_KEY_ROBOTO_CONDENSED_21 - Good for "In the next hour"
+
+static void window_unload(Window *window) {
+    if (icon_bitmap) {
+        gbitmap_destroy(icon_bitmap);
+    }
+
+    text_layer_destroy(temp_layer);
+    bitmap_layer_destroy(icon_layer);
 }
 
-void handle_deinit(void) {
-	  text_layer_destroy(text_layer);
-	  window_destroy(my_window);
+static void init(void) {
+    window = window_create();
+    window_set_background_color(window, GColorBlack);
+    window_set_fullscreen(window, true);
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload
+    });	
+	
+    const int inbound_size = 64;
+    const int outbound_size = 64;
+    app_message_open(inbound_size, outbound_size);
+
+    const bool animated = true;
+    window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+    window_destroy(window);
 }
 
 int main(void) {
-	  handle_init();
-	  app_event_loop();
-	  handle_deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
